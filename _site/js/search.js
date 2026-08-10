20f370816febdf94223f12d6a9de9f2fab2e4add
Christophe Braud <1082222+TofPlay@users.noreply.github.com>
2026-08-10 13:38:47 +0200

    Improve search display

--- _site/js/search.js
+++ _site/js/search.js
@@ -44,10 +44,10 @@ function buildResultHTML(item) {
   let ret = `<div class="pf-result">`;
 
   if (item.parent) {
-    ret += `<a class="pf-result-parent" href="${item.parent_url}">${item.parent}</a><br>`;
+    ret += `<a class="pf-result-parent" href="${fullUrl(item.parent_url)}">${item.parent}</a><br>`;
   }
 
-  ret += `<a class="pf-result-title" href="${item.url}">${item.title}</a>`;
+  ret += `<a class="pf-result-title" href="${fullUrl(item.url)}">${item.title}</a>`;
 
   if (item.excerpt) {
     ret += `<div class="pf-result-excerpt">${item.excerpt}</div>`
